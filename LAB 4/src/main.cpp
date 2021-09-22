10cff44aa46d3bcaf4bb4d5c52259f19f2299308
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-09-22 16:03:59 -0600

    Generación de funciones contador y lcd
    En esta parte se generaron tanto las variables de las funciones de lcd como las del contador

--- LAB 4/src/main.cpp
+++ LAB 4/src/main.cpp
@@ -30,10 +30,23 @@
 //Prototipos de funciones
 //----------------------------------------------------------------------------------------------------------------------
 void LectorVoltajes(void); 
+void Contador(void); 
 //---------------------------------------------------------------------------------------------------------------------
 //Variables Globales
 //----------------------------------------------------------------------------------------------------------------------
+float V1 = 0.0; 
+float V2 = 0.0; 
 
+int decenas1 = 0; 
+int unidades1 = 0; 
+int decimales1 = 0;
+
+int decenas2 = 0; 
+int unidades2 = 0; 
+int decimales2 = 0;
+
+int contador = 0; 
+LiquidCrystal LCD(RS, E, D4, D5, D6, D7);
 //----------------------------------------------------------------------------------------------------------------------
 //ISR  (interrupciones)
 //----------------------------------------------------------------------------------------------------------------------
@@ -43,19 +56,77 @@ void LectorVoltajes(void);
 //----------------------------------------------------------------------------------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  // Este comando limpia la LCD
+  LCD.begin(16, 2);
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 //Loop principal
 //---------------------------------------------------------------------------------------------------------------------
 void loop() {
-  // put your main code here, to run repeatedly:
+  LectorVoltajes();
+  Contador();
+
+  LCD.clear(); //limpia la LCD
+  LCD.print("Pot1:"); //IMPRIMER EN LA LCD
+
+  LCD.setCursor(1, 7); //esta función permite colocar el potenciometro en un espacio específico del LCD
+  LCD.print("Pot2:"); 
+
+  LCD.setCursor(1, 13);
+  LCD.print("CPU:");
+
+  LCD.setCursor(2, 1);
+  LCD.print(decenas1);
+  LCD.print('.');
+  LCD.print(unidades1);
+  LCD.print(decimales1);
+  LCD.print('V');
+
+  LCD.setCursor(2,7);
+  LCD.print(decenas2);
+  LCD.print('.');
+  LCD.print(unidades2);
+  LCD.print(decimales2);
+  LCD.print('V');
+
+  
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 //Lector analogico de voltajes
 //---------------------------------------------------------------------------------------------------------------------
 void LectorVoltajes(void){
-  
+  V1 = analogReadMilliVolts(Voltaje1);
+  V2 = analogReadMilliVolts(Voltaje2);
+
+  //Conversor de decenas, unidades y decimales para V1
+  decenas1 = V1/10;
+  unidades1 = V1 - decenas1*10;
+  decimales1 = (V1*10) - (decenas1*100) - (unidades1*10); 
+
+  //Conversor de decenas, unidades y decimales para V2
+  decenas2 = V2/10;
+  unidades2 = V2 - decenas1*10;
+  decimales2 = (V2*10) - (decenas2*100) - (unidades2*10); 
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+//Contador
+//---------------------------------------------------------------------------------------------------------------------
+void Contador(void){
+  if (digitalRead(Incremento)==1){
+    delay(20);
+    if (digitalRead(Incremento)==0){
+      contador++; 
+    }
+  }
+
+  if (digitalRead(Decremento)==1){
+    delay(20);
+    if (digitalRead(Decremento)==0){
+      contador--; 
+    }
+  }
 }
